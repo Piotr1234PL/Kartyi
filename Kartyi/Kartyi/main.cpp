f38723ef27e7cb1afc7513083704ee89d51b4739
Piotr1234PL <piotr1234pl@gmail.com>
2017-01-11 21:02:00 +0100

    Added 10000 random numbers generator.

--- Kartyi/Kartyi/main.cpp
+++ Kartyi/Kartyi/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <windows.h>
+#include <time.h>
 using namespace std;
 
-int main() {
-	cout << "Hello World" << endl;
+int liczba;
 
+
+
+int main() {
+	srand(time(NULL));
+	cout << "Hello World" << endl;
+	/*for(int i = 15; i >= 0; i--) {
+		Sleep(500);
+		cout << i << endl;
+
+	}*/
+	liczba = rand() % 100 + 1;
+	cout << liczba << endl;
+
+	int pomiary[100][100];
+	for (int i = 0; i < 100; i++) {
+		for (int y = 0; y < 100; y++) {
+			pomiary[i][y] = rand() % 100 + 1;
+			cout << "Pomiar [" << i << "][" << y << "] = " << pomiary[i][y] << endl;
+		}
+	}
+	
 	return 0;
 }
